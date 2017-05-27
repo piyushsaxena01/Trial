bd255ef8de6c1647e79ab757a784e10ea54121f9
Piyush Saxena <piyushsaxena01@gmail.com>
2017-05-27 11:00:44 +0200

    z
    z

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public BoardManager boardscript;
+
+    public static GameManager instance = null;
+
+    private int level = 3;
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        boardscript = GetComponent<BoardManager>();
+        InitGame();
+	}
+	
+    void InitGame()
+    {
+        boardscript.SetupScene(level);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
